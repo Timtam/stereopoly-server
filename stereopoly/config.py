fc6a0c348d7f84a262c480704d811a95b5bb62d3
Toni Barth <software@satoprogs.de>
2018-11-05 22:47:39 +0100

    added test news added path module to retrieve project directory

--- stereopoly/config.py
+++ stereopoly/config.py
@@ -1,5 +1,10 @@
+from .path import get_script_directory
+
+import os.path
+
 class Config(object):
   DEBUG = True
   CSRF_ENABLED = True
 
+NEWS_FILE = os.path.join(get_script_directory(), "var", "news.yml")
 SUPPORTED_APP_VERSION = "1.0.0"
