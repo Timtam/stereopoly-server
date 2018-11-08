d75749aa72f291e53119a7fc6cc6e07f36307869
Toni Barth <software@satoprogs.de>
2018-11-08 11:58:49 +0100

    added new api endpoint for retrieving a single board (not yet implemented)

--- stereopoly/api/boards.py
+++ stereopoly/api/boards.py
@@ -3,8 +3,12 @@ from stereopoly import globals
 from stereopoly.utils import check_app_version
 
 @check_app_version
-def get(api):
+def get_all(api):
   session = globals.DB()
   boards = session.query(Board).all()
   session.close()
   return [{'name': b.name, 'id': b.id, 'version': b.version} for b in boards]
+
+@check_app_version
+def get(api, id):
+  pass
